cf57cd085a85d70b448bcaecc9d3ce38058bccb3
Dan Leonard <daniel@danleonard.us>
2016-10-10 18:12:49 -0500

    Added basic thread code

--- Insomnia/Insomnia/Service1.cs
+++ Insomnia/Insomnia/Service1.cs
@@ -6,12 +6,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Insomnia
 {
     public partial class Service1 : ServiceBase
     {
+        private ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
+        private Thread _thread;
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +23,27 @@ namespace Insomnia
 
         protected override void OnStart(string[] args)
         {
+            _thread = new Thread(WorkerThreadFunc);
+            _thread.Name = "Worker Thread";
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+        private void WorkerThreadFunc()
+        {
+            while (!_shutdownEvent.WaitOne(0))
+            {
+
+            }
         }
 
         protected override void OnStop()
         {
+            _shutdownEvent.Set();
+            if (!_thread.Join(3000)) // give the thread 3 seconds to stop
+            { 
+                _thread.Abort();
+            }
         }
     }
 
