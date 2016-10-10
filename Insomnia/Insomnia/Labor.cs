20bcfdc51a3e9b4d526af95aeecddb0dfb8b86bf
Dan Leonard <daniel@danleonard.us>
2016-10-10 18:20:49 -0500

    Split threaded logic into new file

---
+++ Insomnia/Insomnia/Labor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insomnia
+{
+    public static class ProcessChecker
+    {
+        /// <summary>
+        /// Checks if there exists a currently-running process with some name
+        /// </summary>
+        /// <param name="name">Name of process to look for</param>
+        public static bool IsOpen(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (clsProcess.ProcessName.Contains(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- Insomnia/Insomnia/Service1.cs
+++ Insomnia/Insomnia/Service1.cs
@@ -46,24 +46,4 @@ namespace Insomnia
             }
         }
     }
-
-    public static class ProcessChecker
-    {
-        /// <summary>
-        /// Checks if there exists a currently-running process with some name
-        /// </summary>
-        /// <param name="name">Name of process to look for</param>
-        public static bool IsOpen(string name)
-        {
-            foreach (Process clsProcess in Process.GetProcesses())
-            {
-                if (clsProcess.ProcessName.Contains(name))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    }
 }
